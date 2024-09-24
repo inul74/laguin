0d4f56d73bab5837867210b756bfa177a1ad1923
inul74 <zmusthofa74@gmail.com>
2024-09-24 11:23:51 +0700

    user authentication

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const cookieParser = require("cookie-parser");
  * custom modules
  */
 const login = require("./src/routes/login.route");
+const auth = require("./src/routes/auth.route");
+const authenticatedUser = require("./src/middlewares/auth_user.middleware");
 
 // Initial express app
 const express = require("express");
@@ -39,6 +41,16 @@ app.use(cors()).use(cookieParser());
  * Login page
  */
 app.use("/login", login);
+
+/**
+ * Auth page
+ */
+app.use("/auth", auth);
+
+/**
+ * Check user is authenticated
+ */
+app.use(authenticatedUser);
 
 app.listen(5000, () => {
   console.log(`Server listening att http://localhost:5000`);
